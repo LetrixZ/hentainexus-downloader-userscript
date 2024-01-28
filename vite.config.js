b9208fd20f8efb0f06d04876f74b162d4af24d3d
Fermin Cirella <fermincirella.ar@gmail.com>
2024-01-27 21:28:33 -0300

    Initial version

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import monkey from "vite-plugin-monkey";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    monkey({
+      entry: "src/main.js",
+      userscript: {
+        match: ["https://hentainexus.com/view/*"],
+      },
+    }),
+  ],
+});
